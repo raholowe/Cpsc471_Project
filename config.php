0e7f94fb3f946c5d90df65742895f33e5636badb
lukeatmycode <luke.kissick1@ucalgary.ca>
2018-04-07 16:22:00 -0600

    Changed config.php

--- config.php
+++ config.php
@@ -1,3 +1,17 @@
 <?php
-   $link = mysqli_connect("localhost", "root", "cpsc471", "games");
+	//returns 0 if not connected
+   func gamesConnect() {
+   	 $link = mysqli_connect("localhost", "root", "cpsc471", "games");
+   	 if (mysqli_connect_errno($con))
+   	 {
+   	 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   	 	return 0;
+   	 } else {
+   	 	return $link;
+   	 }
+   }
+
+   func gamesClose($db) {
+   		mysqli_close($db)
+   }
 ?>
--- createUser.php
+++ createUser.php
@@ -1,25 +1,22 @@
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-$time = time();
-$date=  date('Y-m-d', $time) ;
-echo $name. "<br>". $email. "<br>";
-// Create connection
-$con=mysqli_connect("localhost","root","cpsc471","games");
+require('config.php')
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	$time = time();
+	$date=  date('Y-m-d', $time) ;
+	echo $name. "<br>". $email. "<br>";
+	// Create connection
+	$con=gamesConnect();
 // Check connection
-if (mysqli_connect_errno($con))
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-$sql
-= "INSERT INTO USER (username, join_date, community_score, password, email) VALUES ('$name','$date',
-NUll,'$password','$email')";
+
+$sql = "INSERT INTO USER (username, join_date, community_score, password, email) VALUES ('$name','$date',NUll,'$password','$email')";
 if (!mysqli_query($con,$sql))
 {
 die('Error: ' . mysqli_error($con));
 }
 else
 echo "1 record added";
-mysqli_close($con);
+
+gamesClose($con)
 ?>
