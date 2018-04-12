f31c3785bc92c8af1e58e9855d6cc448e6b34f7d
lukeatmycode <luke.kissick1@ucalgary.ca>
2018-04-11 18:02:34 -0600

    reviews are getting fixed

--- view_review_details.php
+++ view_review_details.php
@@ -11,10 +11,19 @@ $sql = "SELECT * FROM REVIEW WHERE REVIEW.game_id = " .$game . " AND REVIEW.user
 $table = mysqli_query($db, $sql);
 $count = mysqli_num_rows($table);
 
+$sql_game = "SELECT GAME.title, GAME.ID FROM GAME WHERE GAME.ID = " . $game;
+
+$table_game = mysqli_query($db,$sql_game);
+
+$game_name = mysqli_fetch_array($table_game);
+
 if($count == 0) {
 	echo "SOMETHING IS WRONG";
 } else {
 	$row = mysqli_fetch_array($table);
+	$goto = "view_game_details.php?key=" . $game_name['ID']; 
+
+	echo "Review of: <a href=" . $goto . ">". $game_name['title'] ."</a><br>";
 	echo "Review by: " . $row['username'] . "<br>";
 	echo "Score: " . $row['score'];
 	echo "<br>";
