f24a079250d7161257fa9270f9aa004174848a76
AbelLeee <32710193+AbelLeee@users.noreply.github.com>
2018-04-12 10:42:35 -0600

    Add files via upload
    Current Visual "upgrades" Still suck ass cause this CSS shit is kicking my ass.

--- welcome.php
+++ welcome.php
@@ -1,74 +1,174 @@
 <?php
 
-	include('session.php');
+   include('session.php');
    function displayTest() {
-   	$link = gamesConnect();
+      $link = gamesConnect();
 
    $result = mysqli_query($link, "SELECT title FROM GAME");
    echo "<table border='1'>
-	<tr>
-	<th>ID</
-	th>
-	<th>Name</th>
-	<th>Publisher</th>
-	</tr>";
-	
-	while($row = mysqli_fetch_array($result))
-	{
-	echo "<tr>";
-	echo "<td>" . $row['ID'] . "</td>";
-	echo "<td>" . $row['title'] . "</td>";
-	echo "<td>" . $row['pub_name'] . "</td>";
-	echo "</tr>";
-	}
-	echo "</table>
-	";
-	gamesClose($link);
-   }
-?>
+   <tr>
+   <th>ID</
+   th>
+   <th>Name</th>
+   <th>Publisher</th>
+   </tr>";
    
-   	<div>
-   		<form action = "searchGames.php" method = "post">
-   			<input type = "text" name = "game" placeholder="Search Games" />
-   			<input type = "submit" value = "Go!"/><br/>
-   		</form>
-   	</div>
-   	<div>
-   		<form action = "searchDevelopers.php" method = "post">
-   			<input type = "text" name = "developer" placeholder="Search Developers"/>
-   			<input type = "submit" value = "Go!"/><br/>
-   		</form>
-	</div>
-	<div>
-   		<form action = "searchPCs.php" method = "post">
-   			<input type = "text" name = "pc" placeholder="Search Playable Characters"/>
-   			<input type = "submit" value = "Go!"/><br/>
-   		</form>
-   	</div>
-   	<div>
-   		<form action = "searchPlatforms.php" method = "post">
-   			<input type = "text" name = "platform" placeholder="Search Platforms"/>
-   			<input type = "submit" value = "Go!"/><br/>
-   		</form>
-   	</div>
-   	<div>
-   		<form action = "searchPublishers.php" method = "post">
-   			<input type = "text" name = "publisher" placeholder="Search Publishers" />
-   			<input type = "submit" value = "Go!"/><br/>
-   		</form>
-   	</div>
-   	<div>
-   		<form action = "searchTags.php" method = "post">
-   			<input type = "text" name = "game" placeholder = "Search Tags"/>
-   			<input type = "submit" value = "Go!"/><br/>
-   		</form>
-   	</div>
+   while($row = mysqli_fetch_array($result))
+   {
+   echo "<tr>";
+   echo "<td>" . $row['ID'] . "</td>";
+   echo "<td>" . $row['title'] . "</td>";
+   echo "<td>" . $row['pub_name'] . "</td>";
+   echo "</tr>";
+   }
+   echo "</table>
+   ";
+   gamesClose($link);
+   }
+
+   function myScore() {
+      $db = gamesConnect();
+         $name = $_SESSION[login_user];
+         $sql = "SELECT Users.community_score FROM Users WHERE Users.username = '$name'";
+         $table = mysqli_query($db, $sql);
+         if(mysqli_num_rows($table) == 1) {
+            $row = mysqli_fetch_array($table);
+            return $row['community_score'];
+         } else {
+            return "Error";
+         }
+         
+   }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title>GameDataBase-CPSC-471</title>
+     <link href="aladincss.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container2">
+   <div id="slider"> 
+      <div class="slides">
+
+<!-- First slide --> 
+         <div class="slider">
+            <div class="legend"></div> 
+            <div class="content"> 
+            <div class="content-txt"> 
+               <h1>
+                  <?php
+                  echo $_SESSION['login_user'] 
+                  ?>'s Profile
+               <h2>
+                  How are you today? 
+                  <p>Today's Date: <span id="datetime"></span></p>
+                     <script>
+                     var dt = new Date();
+                     document.getElementById("datetime").innerHTML = dt.toLocaleDateString() +" " +dt.toLocaleTimeString();
+                     </script>
+               <h2>
+
+            </div> 
+         </div> 
+   <div class="images"> 
+   <img src="IMG/slide1.png"> 
+</div> 
+</div> 
+
+<!-- Second slide --> 
+<div class="slider">
+<div class="legend"></div> 
+<div class="content"> 
+<div class="content-txt"> 
+<h1> Your Current Community Score</h1> 
+<h2>
+   <p> Community Score: 
+      <?php
+      echo myScore();
+      ?>
+   </p>
+
+</h2>
+
+</div> 
+</div> 
+<div class="images"> 
+<img src="IMG/slide2.png"> 
+</div> 
+</div> 
+
+<!-- 3rd Slide -->
+<div class="slider">
+<div class="legend"></div> 
+<div class="content"> 
+<div class="content-txt"> 
+<h1> News</h1> 
+</div> 
+</div> 
+<div class="images"> 
+<img src="IMG/slide3.png"> 
+</div> 
+</div> 
+
+
+
+
+</div> 
+</div>
+
+</div>
+
+
+
+<div class="container1">
+   <h1>
+   Search
+   </h1>
       <div>
-         <form action = "searchCollection.php" method = "post">
-            <input type = "text" name = "game" placeholder = "Search Franchises"/>
-            <input type = "submit" value = "Go!"/><br/>
+         <form action = "searchGames.php" method = "post">
+            <input type = "text" name = "game" placeholder="Search Games" />
+            <button type = "submit">Submit!</button>
          </form>
       </div>
+      <div>
+         <form action = "searchDevelopers.php" method = "post">
+            <input type = "text" name = "developer" placeholder="Search Developers"/>
+            <button type = "submit">Submit!</button>
+         </form>
+   </div>
+   <div>
+         <form action = "searchPCs.php" method = "post">
+            <input type = "text" name = "pc" placeholder="Search Playable Characters"/>
+            <button type = "submit">Submit!</button>
+         </form>
+      </div>
+      <div>
+         <form action = "searchPlatforms.php" method = "post">
+            <input type = "text" name = "platform" placeholder="Search Platforms"/>
+            <button type = "submit">Submit!</button>
+         </form>
+      </div>
+      <div>
+         <form action = "searchPublishers.php" method = "post">
+            <input type = "text" name = "publisher" placeholder="Search Publishers" />
+            <button type = "submit">Submit!</button>
+         </form>
+      </div>
+      <div>
+         <form action = "searchTags.php" method = "post">
+            <input type = "text" name = "game" placeholder = "Search Tags"/>
+            <button type = "submit">Submit!</button>
+         </form>
+      </div>
+   <h1>
+   
+   </h1>
+
+</body>
+</html>
 <?php 
-	include('footer.php') 
+   include('footer.php') 
 ?>
